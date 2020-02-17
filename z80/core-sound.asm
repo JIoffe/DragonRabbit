b1db135ef66ce3627b1c9db37664a6a98855363e
Jason Ioffe <Jason.Ioffe@insight.com>
2020-02-17 17:49:41 -0500

    YM2612 Music

--- z80/core-sound.asm
+++ z80/core-sound.asm
@@ -1,33 +1,105 @@
 ;**********************************************************
 ; Prototype Sound Driver
 ;***********************************************************
+; - Set sfx_callback to define the sound handler for the next frame
+; - Uses VGM format for BGM playback. PSG tones are reserved for sound effects
 ; All the "sound effects" are subroutines that can be referenced
 ; one at a time by the sfx_callback_ptr variable. This can be set
 ; from the M68K side to start a sound effect
 
 write './bin/core-sound-driver.bin'	;WinAPE build target
-psg equ &7F11	; Port to write PSG commands to
+
+; Port to write PSG commands to	
+psg 				equ &7F11
+
+;YM2612 ports
+;part 1
+ym2612a0 			equ &4000
+ym2612d0			equ &4001
+;part 2
+ym2612a1			equ &4002
+ym2612d1			equ &4003
+
+; Constants for VGM file format
+vgm_data_ptr_offset		equ &34
+vgm_sample_rate			equ 44100 ;samples per second
+vgm_samples_per_frame		equ 735
+
+vgm_cmd_2612port0		equ &52
+vgm_cmd_2612port1		equ &53
+
+vgm_cmd_psg			equ &50
+
+vgm_cmd_eof			equ &66
+
+; Waits are in SAMPLES, not frames
+vgm_cmd_wait_n			equ &61
+vgm_cmd_wait_735		equ &62
+vgm_cmd_wait_882		equ &63
+
+; Adds 8bit value of a into the 16bit value of HL (little endian)
+macro AddAHL	offset
+	add 	a, l
+	ld 	l, a
+	adc 	a, h
+	sub 	l
+	ld 	h, a
+mend
+
+macro AddHL	offset
+	ld	a, offset
+	AddAHL
+mend
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; YM2612 Macros
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+macro	WaitYM2612
+	nop
+	nop
+mend
+
+macro	SetYM2612_p0	addr, data
+	ld	a, addr
+	ld	(ym2612a0), a
+	WaitYM2612
+	ld	a, data
+	ld	(ym2612d0), a
+	WaitYM2612
+mend
+
 start:
 	org &0000
 	di
 	jp init
 
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Some variables to take up space before int handler
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-sfx_callback:		dw sfx_nop
-sfx_next_note_ptr:	dw 0
-sfx_melody_start_ptr:	dw 0
-sfx_melody_repeats:	db 0
 sfx_counter		db 0
-sfx_tempo		db 0
-sfx_tempo_accumulator	db 0
-sfx_note_fade		db 0
-sfx_note_counter	db 0
-sfx_note_fade_rate	db 0
-sfx_fade_accumulator	db 0
+sfx_bgm_counter		db 0
 
-align	2
+;bit 0 - playing melody
+;bit 1 - music waiting x frames
+;bit 2 - music should repeat
+sfx_state		db 0
+			ds 1
+
+sfx_callback:		dw sfx_nop	; Callback for sound fx
+
+sfx_music_addr		dw 0		; Beginning addr of current sound file - should be set to valid address if state includes bgm playback
+sfx_music_data		dw 0		; Where to read the actual data for the song. Advances with song.
+
+sfx_YM216_mute_targets:	db 0
+			db 1
+			db 2
+			db 4
+			db 5
+			db 6
+
+align 2
 
 sfx_reset:
 	xor	a
@@ -46,7 +118,6 @@ sfx_mute_psg:
 	ld	a,%11111111
 	ld	(de),a
 	ret
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Sets up a callback that resumes silence of the PSG
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -55,138 +126,220 @@ sfx_nop_trap:
 	ld 	hl,sfx_nop		; enter nop loop
 	ld 	(sfx_callback),hl	
 sfx_nop:
-	jp	sfx_callback_return
+	jp	sfx_return
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; V Int Handler
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 align	&38
 vblank:	
 	push    af
         push    de
         push    hl
 
-	exx
-	ld	hl, sfx_counter
+	ld	hl, sfx_bgm_counter
 	dec	(hl)			; count backwards to check against 0
+
+	ld	a, (sfx_state)
+	bit 	0, a
+	jp	NZ, continue_bgm
+	bgm_return:
+
+	ld	hl, sfx_counter
+	dec	(hl)
+
 	ld	hl, (sfx_callback)
 	jp	(hl)
-sfx_callback_return:
-
+	sfx_return:
+	
 	pop	hl
 	pop	de
 	pop	af
 	ei
 	reti
 
-init:	
-	di
-	im 1
-	ld sp, &2000
-	ei
-endlessloop: 				; sfx advancement happens on vblank
-	jr endlessloop
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Melody Subroutines
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;As a test, PSG melodies have the following format:
-; For each note:
-; 1 byte for the delay
-; 4 bytes for channel volumes
-; 6 bytes for tone channel freq
-; 1 byte for noise channel freq
-; a delay of 0 means the melody is over
-
-sfx_playmelody:
-	xor	a
-	ld	(sfx_melody_repeats),a
-	jr	sfx_melody_init
-sfx_playmelody_repeat:
-	ld	a,1
-	ld	(sfx_melody_repeats),a
-sfx_melody_init:
-	call	sfx_reset
-	ld	hl, (sfx_melody_start_ptr)
-
-	; queue up the first note
-	ld	a, (hl)
-	call	sfx_tempo_adjust
-	ld 	(sfx_note_counter), a
-	inc	hl
-	ld	a, (hl)
-	call	sfx_tempo_adjust
-	ld 	(sfx_note_fade), a
-	inc	hl
-	ld	(sfx_next_note_ptr),hl
-
-	ld 	hl, sfx_playmelody_loop
-	ld 	(sfx_callback),hl
-sfx_playmelody_loop:
-	; Mute active note when it ends
-	ld 	a, (sfx_note_fade)
-	dec	a
-	or	a
-	call	Z, sfx_mute_psg
-	ld 	(sfx_note_fade), a
-
-	; reduce note counter until it hits 0
-	ld	a, (sfx_note_counter)
-	or	a
-	jp	Z, sfx_next_note
-	dec	a
-	ld	(sfx_note_counter), a
-	jp	sfx_callback_return
-sfx_next_note:
-	ld	hl, (sfx_next_note_ptr)
-	ld	de, psg
-	ld	b, 11
-	melody_push_note_loop:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Silence the YM2612 - Key Off on all channels
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+sfx_mute_YM2612:
+	ld	hl, sfx_YM216_mute_targets
+	ld	b, 6
+	sfx_mute_YM2612_loop:
+		ld	a, &28
+		ld	(ym2612a0), a
+		WaitYM2612
 		ld	a, (hl)
-		ld	(de), a
+		ld	(ym2612d0), a
+		WaitYM2612
 		inc	hl
-		djnz	melody_push_note_loop
-
-	; peek the next note
-	ld	a, (hl)
-	or	a
-	; melody ends with 0
-	jr	NZ, sfx_melody_cont
-
-	; see if melody should restart or end
-	ld	a,(sfx_melody_repeats)
-	or	a
-	jp	Z,sfx_nop_trap
-	jp	sfx_melody_init
-
-sfx_melody_cont:
-	; multiply by "tempo"
-	call	sfx_tempo_adjust
-	ld 	(sfx_note_counter), a
-	inc	hl
-
-	ld	a, (hl)
-	call	sfx_tempo_adjust
-	ld 	(sfx_note_fade), a
-	inc	hl
-
-	ld	(sfx_next_note_ptr),hl
-	jp	sfx_callback_return
-sfx_melody_end:
-	ld 	hl, sfx_nop_trap
-	ld 	(sfx_callback),hl
-	jp	sfx_callback_return
-
-; Naively multiplies a by "tempo"
-sfx_tempo_adjust:
-	push	hl
-	ld	hl, sfx_tempo_accumulator
-	ld	(hl), a
-	ld	a, (sfx_tempo)
-	ld	b, a
-	xor	a
-mulu_loop:
-	add	(hl)
-	djnz 	mulu_loop	
-	pop 	hl
+		djnz 	sfx_mute_YM2612_loop
 	ret
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Sets state to play BGM without repetition
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+bgm_norepeat:
+	ld	a, (sfx_state)
+	set	0, a
+	res 	2, a
+	res	1, a
+	ld	(sfx_state), a
+	call	prepare_bgm
+	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Sets state to play BGM with repetition
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+bgm_repeat:
+	ld	a, (sfx_state)
+	set	0, a
+	set 	2, a
+	res	1, a
+	ld	(sfx_state), a
+	call	prepare_bgm
+	ret
+
+init:	
+	im 	1
+	ld 	sp, &2000
+
+	call sfx_mute_psg
+	call sfx_mute_YM2612
+	ei
+
+endlessloop: 				
+	; sfx advancement happens on vblank
+	; this just keeps the CPU occupied
+	jr endlessloop
+
+prepare_bgm:
+	; setup song data ptr
+	call	sfx_mute_YM2612
+
+	ld 	hl, (sfx_music_addr)
+	AddHL	vgm_data_ptr_offset
+	push	hl
+	ld	c, (hl)
+	inc	hl
+	ld	b, (hl)
+	pop	hl
+	add	hl, bc
+	ld	(sfx_music_data), hl
+	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; BGM Playback loop
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+continue_bgm:
+	;a still has sfx_state
+	bit	1, a						; check if waiting
+	jr	Z, bgm_data_scan
+
+	ld	a, (sfx_bgm_counter)
+	or	a
+	jp	NZ, bgm_return
+
+	ld	a, (sfx_state)					; Clear waiting flag
+	res	1, a
+	ld	(sfx_state), a
+
+bgm_data_scan:
+	ld	hl, (sfx_music_data)
+	ld	a, (hl)
+	inc	hl
+	
+	;if/else block to react to supported commands
+	;if space allows, a table might be faster
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; WAIT N SAMPLES - CONVERT TO FRAMES
+	; Not accurate but "Good enough"
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	cp	vgm_cmd_wait_n
+	jr	NZ, vgm_cmd_wait_n_checked
+	; Take the "high" byte and then halve to approximate the # of frames to skip
+	inc	hl
+	ld	a, (hl)
+	srl	a
+	ld	(sfx_bgm_counter), a
+
+	inc	hl
+	ld	(sfx_music_data), hl
+
+	ld	a, (sfx_state)					; set waiting flag
+	set	1, a
+	ld	(sfx_state), a
+	jp	bgm_return
+	vgm_cmd_wait_n_checked:
+
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; YM2612 DATA
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	cp	vgm_cmd_2612port0
+	jr	NZ, vgm_cmd_2612port0_checked
+	ld	a, (hl)
+	ld	(ym2612a0), a
+	WaitYM2612
+	inc	hl
+	ld	a, (hl)
+	ld	(ym2612d0), a
+	WaitYM2612
+	inc	hl
+	ld	(sfx_music_data), hl
+	jp	bgm_data_scan
+	vgm_cmd_2612port0_checked:
+
+	cp	vgm_cmd_2612port1
+	jr	NZ, vgm_cmd_2612port1_checked
+	ld	a, (hl)
+	ld	(ym2612a1), a
+	WaitYM2612
+	inc	hl
+	ld	a, (hl)
+	ld	(ym2612d1), a
+	WaitYM2612
+	inc	hl
+	ld	(sfx_music_data), hl
+	jp	bgm_data_scan
+	vgm_cmd_2612port1_checked:
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; PSG
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; (PSG tones are not supported to make it easier for sound effects...)
+	; maybe in the future I will use a priority queue
+	cp	vgm_cmd_psg
+	jr	NZ, vgm_cmd_psg_checked
+	inc	hl
+	ld	(sfx_music_data), hl
+	jp	bgm_data_scan
+	vgm_cmd_psg_checked:
+	
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; EOF - Clear "playing melody" flag
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	cp	vgm_cmd_eof
+	jr	NZ, vgm_cmd_eof_checked
+	; 	Check if melody is supposed to repeat or not
+	ld	a, (sfx_state)
+	bit	2, a
+	jr	NZ, bgm_eof_repeat
+	res	0, a
+	ld	(sfx_state), a	
+	call	sfx_mute_YM2612
+	jp	bgm_return
+	bgm_eof_repeat:
+	call	NZ, prepare_bgm	; Loop if the "melody repeat" bit is set
+	jp	bgm_return
+	vgm_cmd_eof_checked:
+
+	; anything else is unsupported and hopefully not in the file...
+	; Halt here for debugging ;)
+	ld	bc, 255
+	halt
+	jp	bgm_return
+
+;For sanity, make sure this driver is 16bit aligned
 align 2
 sfx_driver_end:
--- z80/plugin-constants.asm
+++ z80/plugin-constants.asm
@@ -1,41 +1,30 @@
 psg equ &7F11
-sfx_callback		equ &0004
-sfx_callback_return	equ &0044
-sfx_nop_trap		equ &0026
+sfx_callback		equ &0008
+sfx_callback_return	equ &004F
+sfx_nop_trap		equ &0028
 
-sfx_reset		equ &0012
-sfx_mute_psg		equ &0016
-sfx_next_note_ptr	equ &0006
-sfx_melody_start_ptr	equ &0008
-sfx_counter		equ &000B
-sfx_tempo		equ &000C
+sfx_reset		equ &0014
+sfx_mute_psg		equ &0018
 
-sfx_note_fade_rate	equ &0010
-sfx_fade_accumulator	equ &0011
-	
-sfx_playmelody		equ &0053
-sfx_playmelody_repeat	equ &0059
+sfx_music_addr		equ &000A
+sfx_counter		equ &0004
 
-sfx_coredriver_size	equ &00DE
+bgm_repeat		equ &007B
+bgm_norepeat		equ &006B
 
+sfx_coredriver_size	equ &0140
 
-; Macros for triggering melodies
-macro trigger_melody melody,tempo
-	ld	hl,melody
-	ld	(sfx_melody_start_ptr),hl
-
-	ld	a,tempo
-	ld	(sfx_tempo),a
-
-	jp	sfx_playmelody
+; Macros for triggering BGM
+macro	startbgm	song
+	ld	hl, song
+	ld	(sfx_music_addr), hl
+	call	bgm_norepeat
+	jp	sfx_nop_trap
 mend
 
-macro trigger_melody_repeat melody,tempo
-	ld	hl,melody
-	ld	(sfx_melody_start_ptr),hl
-
-	ld	a,tempo
-	ld	(sfx_tempo),a
-
-	jp	sfx_playmelody_repeat
+macro	startbgm_loop	song
+	ld	hl, song
+	ld	(sfx_music_addr), hl
+	call	bgm_repeat
+	jp	sfx_nop_trap
 mend
